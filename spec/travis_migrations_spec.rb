b71e065a049deb0b31f8636af12f2d88d2ab4e53
Renée Hendricksen <renee@travis-ci.org>
2016-05-24 19:33:58 +0200

    create the db on travis

--- spec/travis_migrations_spec.rb
+++ spec/travis_migrations_spec.rb
@@ -3,19 +3,20 @@ require 'yaml'
 
 describe "Travis Migrations Custom Rake Tasks" do
   it "doesn't break on migrate (exits cleanly)" do
-    expect(system('rake db:migrate')).to be_truthy
+    expect(system('RAILS_ENV=test bundle exec rake db:create')).to be_truthy
+    expect(system('RAILS_ENV=test bundle exec rake db:migrate')).to be_truthy
   end
   it "setups the database" do
-    expect(system('RAILS_ENV=test rake db:drop')).to be_truthy
-    expect(system('RAILS_ENV=test rake db:create')).to be_truthy
-    expect(system('RAILS_ENV=test rake db:migrate')).to be_truthy
+    expect(system('RAILS_ENV=test bundle exec rake db:drop')).to be_truthy
+    expect(system('RAILS_ENV=test bundle exec rake db:create')).to be_truthy
+    expect(system('RAILS_ENV=test bundle exec rake db:migrate')).to be_truthy
 
     dbconfig = YAML.load(ERB.new(File.read('config/database.yml')).result)
     ActiveRecord::Base.establish_connection dbconfig['test']
     ActiveRecord::Base.connection_pool.with_connection do |connection|
 
       tables = connection.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';").field_values('table_name')
-      
+
       ["schema_migrations", "tokens", "users", "builds", "repositories",
         "commits", "requests", "ssl_keys", "memberships", "urls",
         "permissions", "jobs", "broadcasts", "emails", "logs", "log_parts",
