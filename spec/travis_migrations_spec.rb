d051068af7077c933c9a03f590d9d82d4e09d412
Sven Fuchs <me@svenfuchs.com>
2016-09-14 19:22:08 +0200

    refactor spec, add a spec for loading structure.sql

--- spec/travis_migrations_spec.rb
+++ spec/travis_migrations_spec.rb
@@ -1,21 +1,37 @@
 require 'spec_helper'
 require 'yaml'
 
-describe "Travis Migrations Custom Rake Tasks" do
-  it "setups the database with the correct tables" do
-    dbconfig = YAML.load(ERB.new(File.read('config/database.yml')).result)
-    ActiveRecord::Base.establish_connection dbconfig['test']
-    ActiveRecord::Base.connection_pool.with_connection do |connection|
 
-      tables = connection.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';").field_values('table_name')
+describe 'Rake tasks' do
+  let(:config) { YAML.load(ERB.new(File.read('config/database.yml')).result) }
+  let(:conn)   { ActiveRecord::Base.connection }
+  let(:tables) { conn.select_values("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'") }
+  before       { ActiveRecord::Base.establish_connection(config['test']) }
 
-      ["schema_migrations", "tokens", "users", "builds", "repositories",
-        "commits", "requests", "ssl_keys", "memberships", "urls",
-        "permissions", "jobs", "broadcasts", "emails", "logs", "log_parts",
-        "organizations", "annotation_providers", "annotations", "branches",
-        "stars", "crons", "subscriptions", "plans", "coupons", "stripe_events", "invoices"].each do |table_name|
-        expect(tables).to include(table_name)
-      end
+  def run(cmd)
+    system "RAILS_ENV=test bundle exec #{cmd}"
+    expect($?.exitstatus).to eq 0
+  end
+
+  shared_examples_for 'creates the expected tables' do
+    it 'creates the expected tables' do
+      expect(tables.sort).to eq %w(
+        schema_migrations tokens users builds repositories commits requests
+        ssl_keys memberships urls permissions jobs broadcasts emails logs
+        log_parts organizations annotation_providers annotations branches stars
+        crons subscriptions plans coupons stripe_events invoices
+      ).sort
     end
+  end
+
+  describe 'rake db:create' do
+    before { run 'rake db:drop db:create db:migrate' }
+    include_examples 'creates the expected tables'
+
+  end
+
+  describe 'rake db:schema:load' do
+    before { run 'rake db:drop db:create db:structure:load' }
+    include_examples 'creates the expected tables'
   end
 end
