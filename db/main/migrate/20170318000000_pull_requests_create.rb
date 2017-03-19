637f36c583711d083be1fc94d46df7944e366909
Sven Fuchs <me@svenfuchs.com>
2017-03-19 14:51:14 +0100

    create pull_requests table

---
+++ db/main/migrate/20170318000000_pull_requests_create.rb
@@ -0,0 +1,14 @@
+class PullRequestsCreate < ActiveRecord::Migration
+  def change
+    create_table :pull_requests do |t|
+      t.belongs_to :repository
+      t.integer :number
+      t.string  :title
+      t.string  :state
+      t.integer :head_repo_github_id
+      t.string  :head_repo_slug
+      t.string  :head_ref
+      t.timestamps
+    end
+  end
+end
