f4c12814b11f56c8bb7a6487fe2cf1a6b62172f9
Dan Buch <dan@travis-ci.org>
2017-04-06 17:10:40 -0400

    Remove logs migrations and references to them
    in favor of all travis-logs migrations being managed by travis-logs.
    See travis-ci/travis-logs#97.

--- spec/travis_migrations_spec.rb
+++ spec/travis_migrations_spec.rb
@@ -13,27 +13,13 @@ describe 'Rake tasks' do
     pull_requests stages
     )
   }
-  let(:expected_logs_tables){ %w(
-    schema_migrations logs log_parts
-    )
-  }
 
   def run(cmd)
     system "RAILS_ENV=test bundle exec #{cmd}"
     expect($?.exitstatus).to eq 0
   end
 
-  def run_logs(cmd)
-    system "RAILS_ENV=test_logs bundle exec #{cmd}"
-    expect($?.exitstatus).to eq 0
-  end
-
   describe 'rake db:create' do
-    it 'migrates the logs db' do
-      run_logs 'rake db:drop db:create db:migrate'
-      ActiveRecord::Base.establish_connection(config['test_logs'])
-      expect(tables.sort).to eq expected_logs_tables.sort
-    end
     it 'migrates the main db' do
       run 'rake db:drop db:create db:migrate'
       ActiveRecord::Base.establish_connection(config['test'])
@@ -42,11 +28,6 @@ describe 'Rake tasks' do
   end
 
   describe 'rake db:schema:load' do
-    it 'loads the logs schema' do
-      run_logs 'rake db:drop db:create db:structure:load'
-      ActiveRecord::Base.establish_connection(config['test_logs'])
-      expect(tables.sort).to eq expected_logs_tables.sort
-    end
     it 'loads the main schema'do
     run 'rake db:drop db:create db:structure:load'
     ActiveRecord::Base.establish_connection(config['test'])
