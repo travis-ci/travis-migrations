3dffb187e5e249ef3376c9915cb682867657e17f
Tyranja <tyranja@cassiopeia.uberspace.de>
2017-04-19 12:18:30 +0200

    Add a new default

--- db/main/migrate/20170419093249_add_source_to_subscriptions.rb
+++ db/main/migrate/20170419093249_add_source_to_subscriptions.rb
@@ -1,5 +1,5 @@
 class AddSourceToSubscriptions < ActiveRecord::Migration
   def change
-    add_column :source, :string, :null => false
+    add_column :source, :string, :default => 'manual', :null => false
   end
 end
