09085d6098809cdbd115e8f222994ceb9ad709b3
Tyranja <tyranja@cassiopeia.uberspace.de>
2017-04-20 14:26:15 +0200

    Add another type of source

--- db/main/migrate/20170419093249_add_source_to_subscriptions.rb
+++ db/main/migrate/20170419093249_add_source_to_subscriptions.rb
@@ -1,9 +1,9 @@
 class AddSourceToSubscriptions < ActiveRecord::Migration
   def up
     execute <<-SQL
-      CREATE TYPE source_type AS ENUM ('manual', 'stripe', 'github');
+      CREATE TYPE source_type AS ENUM ('manual', 'stripe', 'github', 'unknown');
     SQL
-    add_column :subscriptions, :source, :source_type, default: 'manual', null: false
+    add_column :subscriptions, :source, :source_type, default: 'unknown', null: false
   end
 
   def down
