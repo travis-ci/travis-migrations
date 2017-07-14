d7a7ee9cdc858360ed39f273253c4a7c4ca9450e
Tyranja <tyranja@cassiopeia.uberspace.de>
2017-07-14 11:18:37 +0200

    rename the column and fix typo

--- db/main/migrate/20170713162000_add_cc_last_digits_to_invoices.rb
+++ db/main/migrate/20170713162000_add_cc_last_digits_to_invoices.rb
@@ -1,7 +1,7 @@
-class AddLastCcDigistsToInvoices < ActiveRecord::Migration
+class AddCcLastDigitsToInvoices < ActiveRecord::Migration
   def change
     change_table :invoices do |t|
-      t.string :last_cc_digists
+      t.string :cc_last_digits
     end
   end
 end
