65ec3e411d40cdfca31f95597e37f245962305d6
Piotr Sarnacki <drogus@gmail.com>
2018-04-12 13:54:18 +0200

    Add file

---
+++ script/upload_structure.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require "s3"
+
+path_to_structure = ARGV[0] || 'db/main/structure.sql'
+path = File.expand_path("../../#{path_to_structure}", __FILE__)
+structure = File.read(path)
+
+service = S3::Service.new(access_key_id: ENV['ARTIFACTS_KEY'],
+                          secret_access_key: ['ARTIFACTS_SECRET'])
+
+bucket = service.buckets.find("travis-migrations-structure-dumps")
+object = bucket.objects.build("structure-#{ENV['TRAVIS_BUILD_NUMBER']}.sql")
+object.content = structure
+object.acl = :public_read
+object.save
+
+puts "structure.sql was saved and uploaded to S3:\n#{object.url}"
