d9965858740b3cd4221bc2ac80899edc883f4ffe
Sven Fuchs <me@svenfuchs.com>
2018-05-17 19:03:04 +0200

    add indexes on config_id on requests, builds, and jobs

---
+++ db/main/migrate/20180517000002_index_jobs_config_id.rb
@@ -0,0 +1,11 @@
+class IndexJobsConfigId < ActiveRecord::Migration[4.2]
+  self.disable_ddl_transaction!
+
+  def up
+    execute "CREATE INDEX CONCURRENTLY index_jobs_on_config_id ON requests (config_id)"
+  end
+
+  def down
+    execute "DROP INDEX CONCURRENTLY index_jobs_on_config_id"
+  end
+end
