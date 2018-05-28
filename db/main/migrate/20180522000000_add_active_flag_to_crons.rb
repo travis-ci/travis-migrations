3723ac905557ed9cb6ea10ca17ae2b2145ab0824
Aakriti Gupta <aakriti@travis-ci.org>
2018-05-28 19:35:28 +0200

    fixup

--- db/main/migrate/20180522000000_add_active_flag_to_crons.rb
+++ db/main/migrate/20180522000000_add_active_flag_to_crons.rb
@@ -2,8 +2,10 @@ class AddActiveFlagToCrons < ActiveRecord::Migration[4.2]
   self.disable_ddl_transaction!
 
   def change
-    change_table :crons do |t|
-      t.boolean :active, default: true
+    unless column_exists?(:crons, :active)
+      change_table :crons do |t|
+        t.boolean :active, default: true
+      end
     end
 
     add_index :crons, :next_run, where: "(active IS TRUE)", algorithm: :concurrently
