1bed2e2c82854ca648277c0b4e1e6384fea1f014
Sven Fuchs <me@svenfuchs.com>
2018-06-06 16:20:46 +0200

    index [commits|jobs]_on_repository_id

---
+++ db/main/migrate/20180606000000_index_commits_on_repository_id.rb
@@ -0,0 +1,11 @@
+class IndexCommitsOnRepositoryId < ActiveRecord::Migration[4.2]
+  self.disable_ddl_transaction!
+
+  def up
+    execute "CREATE INDEX CONCURRENTLY IF NOT EXISTS index_commits_on_repository_id ON commits (repository_id)"
+  end
+
+  def down
+    execute "DROP INDEX CONCURRENTLY index_commits_on_repository_id"
+  end
+end
