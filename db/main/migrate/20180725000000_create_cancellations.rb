08f69c1eddf595cc0108d17fa7b419250f8debea
Sergio Gil <sgilperez@gmail.com>
2018-07-25 17:37:34 +0200

    Add cancellations table

---
+++ db/main/migrate/20180725000000_create_cancellations.rb
@@ -0,0 +1,13 @@
+class CreateCancellations < ActiveRecord::Migration[4.2]
+  def change
+    create_table :cancellations do |t|
+      t.references :subscription, index: true, null: false
+      t.references :user
+      t.string :plan, null: false
+      t.date :subscription_start_date, :cancellation_date, null: false
+      t.string :reason
+      t.text :reason_details
+      t.timestamps
+    end
+  end
+end
