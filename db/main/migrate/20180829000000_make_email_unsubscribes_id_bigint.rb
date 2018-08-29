1416f46734fba24fbeba212ee862cc25fb22a087
Sergio Gil <sgilperez@gmail.com>
2018-08-29 12:49:28 +0200

    Change email_unsubscribes.id to bigint

---
+++ db/main/migrate/20180829000000_make_email_unsubscribes_id_bigint.rb
@@ -0,0 +1,9 @@
+class MakeEmailUnsubscribesIdBigint < ActiveRecord::Migration[4.2]
+  def up
+    change_column :email_unsubscribes, :id, :bigint
+  end
+
+  def down
+    change_column :email_unsubscribes, :id, :integer
+  end
+end
