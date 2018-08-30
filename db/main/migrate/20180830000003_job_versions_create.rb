ab4292bb37936dfb77e15d58b933130e1161f734
Sven Fuchs <me@svenfuchs.com>
2018-08-30 14:20:15 +0200

    add [builds|jobs].restarted_at, create job_versions

---
+++ db/main/migrate/20180830000003_job_versions_create.rb
@@ -0,0 +1,15 @@
+class JobVersionsCreate < ActiveRecord::Migration[4.2]
+  def change
+    create_table :job_versions do |t|
+      t.integer  :job_id
+      t.integer  :number
+      t.string   :state
+      t.datetime :created_at
+      t.datetime :queued_at
+      t.datetime :received_at
+      t.datetime :started_at
+      t.datetime :finished_at
+      t.datetime :restarted_at
+    end
+  end
+end
