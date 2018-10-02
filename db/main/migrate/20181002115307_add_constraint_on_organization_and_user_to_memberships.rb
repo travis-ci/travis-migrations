cdc361f90df593efa4fe573c80100662e44cdc27
Curtis Ekstrom <code@fauxton.io>
2018-10-02 14:55:35 +0200

    Break up migration: delete first, apply index second

--- db/main/migrate/20181002115307_add_constraint_on_organization_and_user_to_memberships.rb
+++ db/main/migrate/20181002115307_add_constraint_on_organization_and_user_to_memberships.rb
@@ -3,16 +3,6 @@ class AddConstraintOnOrganizationAndUserToMemberships < ActiveRecord::Migration[
 
   def up
     execute <<~sql
-DELETE FROM memberships WHERE id in (
-	SELECT a.id FROM
-		(SELECT m.id, row_number() OVER (ORDER BY m.id) as rnum
-     FROM (SELECT user_id, organization_id
-           FROM memberships
-           GROUP BY (user_id, organization_id)
-           HAVING count(*) > 1) t
-           INNER JOIN memberships m ON m.user_id = t.user_id AND m.organization_id = t.organization_id) AS a
-  WHERE mod(a.rnum, 2) = 0);
-
 CREATE UNIQUE INDEX CONCURRENTLY index_organization_id_and_user_id_on_memberships ON memberships USING btree (organization_id, user_id);
     sql
   end
