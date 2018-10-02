cafb95aef8289bf479fc9c9d4d551e3436811cc7
Curtis Ekstrom <code@fauxton.io>
2018-10-02 14:11:56 +0200

    Add unique index to memberships to avoid duplicate records
    The memberships table does not currently have any constraints/indexes to
    prevent duplicate entries, e.g. it is theoretically possible to have a
    membership record where a user is a member and admin simultaneously, or
    two admin/member records;
    
    Before this was applied, we had a small amount of records keeping us
    from applying the index. The first part of the migration therefore deletes those
    offending records; the second part then creates the index.
    
    Dropping the migration will remove the index.

--- db/main/migrate/20181002115307_add_constraint_on_organization_and_user_to_memberships.rb
+++ db/main/migrate/20181002115307_add_constraint_on_organization_and_user_to_memberships.rb
@@ -1,4 +1,23 @@
-class AddConstraintOnOrganizationAndUserToMemberships < ActiveRecord::Migration[5.2]
-  def change
+class AddConstraintOnOrganizationAndUserToMemberships < ActiveRecord::Migration[4.2]
+  def up
+    execute <<~sql
+DELETE FROM memberships WHERE id in (
+	SELECT a.id FROM
+		(SELECT m.id, row_number() OVER (ORDER BY m.id) as rnum
+     FROM (SELECT user_id, organization_id
+           FROM memberships
+           GROUP BY (user_id, organization_id)
+           HAVING count(*) > 1) t
+           INNER JOIN memberships m ON m.user_id = t.user_id AND m.organization_id = t.organization_id) AS a
+  WHERE mod(a.rnum, 2) = 0);
+
+CREATE UNIQUE INDEX CONCURRENTLY index_organization_id_and_user_id_on_memberships ON memberships USING btree (organization_id, user_id);
+    sql
+  end
+
+  def down
+    execute <<~sql
+DROP INDEX CONCURRENTLY index_organization_id_and_user_id_on_memberships;
+		sql
   end
 end
