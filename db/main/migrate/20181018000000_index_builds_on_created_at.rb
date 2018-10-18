28d24ef40d466db7ad8aa57fde5063ca0d9684ae
Sven Fuchs <me@svenfuchs.com>
2018-10-18 15:00:36 +0200

    index builds on created_at

---
+++ db/main/migrate/20181018000000_index_builds_on_created_at.rb
@@ -0,0 +1,11 @@
+class IndexBuildsOnCreatedAt < ActiveRecord::Migration[4.2]
+  self.disable_ddl_transaction!
+
+  def up
+    execute 'CREATE INDEX CONCURRENTLY IF NOT EXISTS index_builds_on_created_at ON builds (created_at)'
+  end
+
+  def down
+    execute 'DROP INDEX CONCURRENTLY index_builds_on_created_at'
+  end
+end
