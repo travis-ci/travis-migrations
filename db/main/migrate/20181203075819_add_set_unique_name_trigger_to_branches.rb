bace53c89630d46f6655d76e3c0f30851b9f1a5f
Piotr Sarnacki <drogus@gmail.com>
2018-12-04 11:14:01 +0100

    Split unique_name trigger and adding unique name into 2 migrations
    At the moment gatekeeper code is creating branch duplicates so as soon
    as we add a `set_unique_name` trigger to branches, we will have
    duplicates in unique column and thus we won't be able to create an
    index. That's why I'm splitting it into 2 files here, so I can do it in
    a sequence:
    
    * add `unique_name` column
    * create a unique index on the `unique_name` column
    * install the trigger

--- db/main/migrate/20181203075819_add_set_unique_name_trigger_to_branches.rb
+++ db/main/migrate/20181203075819_add_set_unique_name_trigger_to_branches.rb
@@ -1,11 +1,9 @@
 class AddSetUniqueNameTriggerToBranches < ActiveRecord::Migration[5.2]
   def up
-    add_column :branches, :unique_name, :text
     execute File.read(Rails.root.join('db/main/sql/triggers/create_set_unique_name.sql'))
   end
 
   def down
     execute File.read(Rails.root.join('db/main/sql/triggers/drop_set_unique_name.sql'))
-    remove_column :branches, :unique_name
   end
 end
---
+++ db/main/migrate/20181204111031_add_unique_name_column_to_branches.rb
@@ -0,0 +1,5 @@
+class AddUniqueNameColumnToBranches < ActiveRecord::Migration[5.2]
+  def change
+    add_column :branches, :unique_name, :text
+  end
+end
