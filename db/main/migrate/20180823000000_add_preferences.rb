6ecd06c324a1d24aa35b618a0e989c73a1782fe9
Sergio Gil <sgilperez@gmail.com>
2019-01-02 16:31:11 +0100

    Extract Postgres version helpers

--- db/main/migrate/20180823000000_add_preferences.rb
+++ db/main/migrate/20180823000000_add_preferences.rb
@@ -1,16 +1,9 @@
 class AddPreferences < ActiveRecord::Migration[4.2]
+  include Travis::PostgresVersion
+
   def change
     change_table :users do |t|
       t.column :preferences, json_type
     end
-  end
-
-  def json_type
-    postgres_version == '9.3' ? :json : :jsonb
-  end
-
-  def postgres_version
-    full = ActiveRecord::Base.connection.select_value('SELECT version()')
-    full[/^PostgreSQL (\d+\.\d+)/, 1]
   end
 end
--- db/main/migrate/20190102000000_add_organization_preferences.rb
+++ db/main/migrate/20190102000000_add_organization_preferences.rb
@@ -1,16 +1,9 @@
 class AddOrganizationPreferences < ActiveRecord::Migration[5.2]
+  include Travis::PostgresVersion
+
   def change
     change_table :organizations do |t|
       t.column :preferences, json_type
     end
-  end
-
-  def json_type
-    postgres_version == '9.3' ? :json : :jsonb
-  end
-
-  def postgres_version
-    full = ActiveRecord::Base.connection.select_value('SELECT version()')
-    full[/^PostgreSQL (\d+\.\d+)/, 1]
   end
 end
