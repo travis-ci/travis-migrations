e712fd422ec1a393e0b08e3d701707e7f36cf7d0
Curtis Ekstrom <code@fauxton.io>
2019-01-17 14:06:43 +0100

    Constantize generated class name

--- db/main/migrate/20181205152712_add_unique_index_to_crons_on_branch_id.rb
+++ db/main/migrate/20181205152712_add_unique_index_to_crons_on_branch_id.rb
@@ -1,4 +1,4 @@
-class add_unique_index_to_crons_on_branch_id < ActiveRecord::Migration[4.2]
+class AddUniqueIndexToCronsOnBranchId < ActiveRecord::Migration[4.2]
   self.disable_ddl_transaction!
 
   def up
