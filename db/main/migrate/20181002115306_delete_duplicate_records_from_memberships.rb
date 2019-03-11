70c5b3a0e904f5cd0ce74d44bba4a4abd539aa21
Phil Peble <phil@travis-ci.com>
2019-03-11 15:00:54 -0500

    Remove parens from GROUP_BY clause in memberships cleanup migration
    For reasons that escape me the parens in the GROUP_BY of this migration
    cause an error on postgres 9.3 but work just fine on postgres 9.6.
    Behavior is not changed.

--- db/main/migrate/20181002115306_delete_duplicate_records_from_memberships.rb
+++ db/main/migrate/20181002115306_delete_duplicate_records_from_memberships.rb
@@ -6,7 +6,7 @@ DELETE FROM memberships WHERE id in (
 		(SELECT m.id, row_number() OVER (ORDER BY m.id) as rnum
      FROM (SELECT user_id, organization_id
            FROM memberships
-           GROUP BY (user_id, organization_id)
+           GROUP BY user_id, organization_id
            HAVING count(*) > 1) t
            INNER JOIN memberships m ON m.user_id = t.user_id AND m.organization_id = t.organization_id) AS a
   WHERE mod(a.rnum, 2) = 0);
