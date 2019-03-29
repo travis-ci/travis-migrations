df42b14e05d7f86c866a8fcc99febcd0fe3aa80f
Piotr Sarnacki <drogus@gmail.com>
2019-03-29 10:49:34 +0100

    Add gatekeeper_workers

---
+++ db/main/migrate/20190329093854_create_gatekeeper_workers.rb
@@ -0,0 +1,12 @@
+class CreateGatekeeperWorkers < ActiveRecord::Migration[5.2]
+  def up
+    create_table :gatekeeper_workers do |t|
+    end
+
+    execute "INSERT INTO gatekeeper_workers(id) SELECT generate_series(1,200) as id;"
+  end
+
+  def down
+    drop_table :gatekeeper_workers
+  end
+end
