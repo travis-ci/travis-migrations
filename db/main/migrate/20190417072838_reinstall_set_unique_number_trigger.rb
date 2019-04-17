34bfedc8978db58ce18570cbe6f6df53f4f2954c
Piotr Sarnacki <drogus@gmail.com>
2019-04-17 07:47:15 +0200

    Allow to skip setting unique_number (#181)
    Initially I wanted to not update unique_number when performing an UPDATE
    to make things simple but after giving it more thought I decided that
    it's better to be able to do it on both UPDATE and INSERT. Normally we
    don't update builds.number but it might still be valuable to have the
    consistency when doing some kind of meintenance (like renumbering
    builds).

---
+++ db/main/migrate/20190417072838_reinstall_set_unique_number_trigger.rb
@@ -0,0 +1,9 @@
+class ReinstallSetUniqueNumberTrigger < ActiveRecord::Migration[5.2]
+  def up
+    execute File.read(Rails.root.join('db/main/sql/triggers/drop_set_unique_number.sql'))
+    execute File.read(Rails.root.join('db/main/sql/triggers/create_set_unique_number.sql'))
+  end
+
+  def down
+  end
+end
