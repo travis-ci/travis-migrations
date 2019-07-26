c0b2d638b1547720a7d416dd5c3a5f5000224bfc
Piotr Sarnacki <drogus@gmail.com>
2019-07-26 14:09:03 +0200

    Only create deleted_* tables if they don't exist

--- db/main/migrate/20190613120000_soft_delete_repository_along_with_its_data.rb
+++ db/main/migrate/20190613120000_soft_delete_repository_along_with_its_data.rb
@@ -1,17 +1,17 @@
 class SoftDeleteRepositoryAlongWithItsData < ActiveRecord::Migration[5.2]
   def up
-    execute "create table deleted_builds (like builds)"
-    execute "create table deleted_stages (like stages)"
-    execute "create table deleted_jobs (like jobs)"
-    execute "create table deleted_requests (like requests)"
-    execute "create table deleted_commits (like commits)"
-    execute "create table deleted_pull_requests (like pull_requests)"
-    execute "create table deleted_job_configs (like job_configs)"
-    execute "create table deleted_build_configs (like build_configs)"
-    execute "create table deleted_request_configs (like request_configs)"
-    execute "create table deleted_request_payloads (like request_payloads)"
-    execute "create table deleted_ssl_keys (like ssl_keys)"
-    execute "create table deleted_tags (like tags)"
+    execute "create table if not exists deleted_builds (like builds)"
+    execute "create table if not exists deleted_stages (like stages)"
+    execute "create table if not exists deleted_jobs (like jobs)"
+    execute "create table if not exists deleted_requests (like requests)"
+    execute "create table if not exists deleted_commits (like commits)"
+    execute "create table if not exists deleted_pull_requests (like pull_requests)"
+    execute "create table if not exists deleted_job_configs (like job_configs)"
+    execute "create table if not exists deleted_build_configs (like build_configs)"
+    execute "create table if not exists deleted_request_configs (like request_configs)"
+    execute "create table if not exists deleted_request_payloads (like request_payloads)"
+    execute "create table if not exists deleted_ssl_keys (like ssl_keys)"
+    execute "create table if not exists deleted_tags (like tags)"
 
     execute File.read(Rails.root.join('db/main/sql/create_soft_delete_repo_data.sql'))
   end
