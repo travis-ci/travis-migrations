733e0418782efd95db1472fd13a87e5d4e1d238f
Artur Kremens <artur@assembla.com>
2019-09-14 21:01:18 +0200

    Update vcs id in batches

--- db/main/migrate/20190913092543_copy_vcs_id_for_repository.rb
+++ db/main/migrate/20190913092543_copy_vcs_id_for_repository.rb
@@ -2,6 +2,17 @@ class CopyVcsIdForRepository < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def up
-    execute(%Q[UPDATE "repositories" SET vcs_id = github_id])
+    last_id = select_value('SELECT id FROM repositories ORDER BY id DESC LIMIT 1') || 0
+    batch_size = 5000
+    (0..last_id).step(batch_size).each do |from_id|
+      to_id = from_id + batch_size
+
+      ActiveRecord::Base.transaction do
+        execute(%Q[UPDATE "repositories" SET vcs_id = github_id WHERE id BETWEEN #{from_id} AND #{to_id}])
+      end
+    end
+  end
+
+  def down
   end
 end
