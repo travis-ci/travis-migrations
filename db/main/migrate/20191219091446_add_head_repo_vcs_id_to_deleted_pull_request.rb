0847930a2bdf97ee151e41177221221293c557d7
Artur Kremens <artur@assembla.com>
2019-12-19 09:42:48 +0100

    Add head_repo_vcs_id for (deleted) pull requests

---
+++ db/main/migrate/20191219091446_add_head_repo_vcs_id_to_deleted_pull_request.rb
@@ -0,0 +1,15 @@
+class AddHeadRepoVcsIdToDeletedPullRequest < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
+  def up
+    add_column :deleted_pull_requests, :head_repo_vcs_id, :string, default: nil
+
+    execute "CREATE INDEX CONCURRENTLY index_deleted_pull_requests_on_head_repo_vcs_id ON deleted_pull_requests (head_repo_vcs_id);"
+  end
+
+  def down
+    ActiveRecord::Base.transaction do
+      remove_column :deleted_pull_requests, :head_repo_vcs_id
+    end
+  end
+end
