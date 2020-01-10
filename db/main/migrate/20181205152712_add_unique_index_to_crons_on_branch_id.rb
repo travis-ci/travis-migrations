d30aa739610038558db00e01458ac1d4588abbdd
Tahsin Hasan <tahsin@travis-ci.org>
2020-01-10 21:01:14 +0600

    fix for duplicate cron job

---
+++ db/main/migrate/20181205152712_add_unique_index_to_crons_on_branch_id.rb
@@ -0,0 +1,12 @@
+class AddUniqueIndexToCronsOnBranchId < ActiveRecord::Migration[4.2]
+    self.disable_ddl_transaction!
+  
+    def up
+      execute "DROP INDEX CONCURRENTLY index_crons_on_branch_id"
+      execute "CREATE UNIQUE INDEX CONCURRENTLY index_crons_on_branch_id ON crons(branch_id)"
+    end
+  
+    def down
+      execute "DROP INDEX CONCURRENTLY index_crons_on_branch_id"
+    end
+  end
