78cca966f23200d375ef4f8e73c28cf1db7f1b2a
Tahsin Hasan <tahsin@travis-ci.org>
2020-01-13 21:14:58 +0600

    fix build error

--- db/main/migrate/20181205152712_add_unique_index_to_crons_on_branch_id.rb
+++ db/main/migrate/20181205152712_add_unique_index_to_crons_on_branch_id.rb
@@ -2,7 +2,7 @@ class AddUniqueIndexToCronsOnBranchId < ActiveRecord::Migration
     self.disable_ddl_transaction!
   
     def up
-      execute "DROP INDEX CONCURRENTLY index_crons_on_branch_id"
+      execute "DROP INDEX CONCURRENTLY IF EXISTS index_crons_on_branch_id"
       execute "CREATE UNIQUE INDEX CONCURRENTLY index_crons_on_branch_id ON crons(branch_id)"
     end
   
