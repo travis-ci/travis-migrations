fdad6cbcee971e7c1f6f153eea0ef66f1a62b809
Curtis Ekstrom <code@fauxton.io>
2020-02-14 16:15:12 +0100

    Add index to job_versions on job_id (#210)

---
+++ db/main/migrate/20200214144655_add_index_to_job_versions_on_job_id.rb
@@ -0,0 +1,7 @@
+class AddIndexToJobVersionsOnJobId < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
+  def change
+    add_index :job_versions, :job_id, algorithm: :concurrently
+  end
+end
