bd32067e328dcd0485c97d61ab4c1617096d1438
Curtis Ekstrom <code@fauxton.io>
2020-04-06 14:16:34 +0200

    Add final index migration
    This adds a sorted index by repository_id, which should speed up queries that do not care about the `state` of jobs.

---
+++ db/main/migrate/20200406121218_add_index_jobs_on_repository_id_order_by_newest.rb
@@ -0,0 +1,11 @@
+class AddIndexJobsOnRepositoryIdOrderByNewest < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
+  def up
+    execute "CREATE INDEX CONCURRENTLY IF NOT EXISTS index_jobs_on_repository_id_order_by_newest on jobs (repository_id, id desc)"
+  end
+
+  def down
+    execute "DROP INDEX CONCURRENTLY index_jobs_on_repository_id_order_by_newest"
+  end
+end
--- db/main/structure.sql
+++ db/main/structure.sql
@@ -4217,7 +4217,7 @@ CREATE INDEX index_jobs_on_repository_id ON public.jobs USING btree (repository_
 
 
 --
--- Name: index_jobs_on_repository_id_non_queued_order_by_newest; Type: INDEX; Schema: public; Owner: -
+-- Name: index_jobs_on_repository_id_order_by_newest; Type: INDEX; Schema: public; Owner: -
 --
 
 CREATE INDEX index_jobs_on_repository_id_non_queued_order_by_newest ON public.jobs USING btree (repository_id, id DESC) WHERE ((state)::text = ANY (('{passed,started,errored,failed,canceled}'::character varying[])::text[]));
@@ -5796,6 +5796,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20200316085738'),
 ('20200325115329'),
 ('20200325130013'),
-('20200330110527');
+('20200330110527'),
+('20200406121218');
 
 
