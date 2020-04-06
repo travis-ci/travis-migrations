642c34aa8facab792144134acdb94f380eda04a0
Curtis Ekstrom <code@fauxton.io>
2020-04-06 13:50:57 +0200

    Rename functions and improve implementation

--- db/main/migrate/20200330110527_add_function_most_recent_job_ids_for_repository_by_state.rb
+++ db/main/migrate/20200330110527_add_function_most_recent_job_ids_for_repository_by_state.rb
@@ -2,15 +2,11 @@ class AddFunctionMostRecentJobIdsForRepositoryByState < ActiveRecord::Migration[
   def up
     execute """
     CREATE OR REPLACE FUNCTION
-      most_recent_job_ids_for_repository_by_state(repository_id int, states varchar) RETURNS table (id int) AS
+      most_recent_job_ids_for_repository_by_state(rid int, st varchar) RETURNS table (job_id int, repository_id int) AS
       $BODY$
     DECLARE
     BEGIN
-      RETURN QUERY EXECUTE format(
-        'SELECT j.id from jobs j where repository_id = %s and state = any(''{%s}'') order by id desc limit 100',
-        repository_id,
-        states
-      );
+      RETURN QUERY select j.id, j.repository_id from jobs j where j.repository_id = rid and j.state = st order by j.id desc limit 100;
     END
     $BODY$
     LANGUAGE plpgsql;
