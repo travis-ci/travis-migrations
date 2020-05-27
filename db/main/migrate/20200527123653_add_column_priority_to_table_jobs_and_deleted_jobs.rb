36cae899f7326addf17a8bf64bbbaf795ffeedf3
vijaycel <vvelusamy@celestialsys.com>
2020-05-28 00:32:02 +0530

    Add priority column to jobs and deleted_jobs tables

---
+++ db/main/migrate/20200527123653_add_column_priority_to_table_jobs_and_deleted_jobs.rb
@@ -0,0 +1,13 @@
+class AddColumnPriorityToTableJobsAndDeletedJobs < ActiveRecord::Migration[5.2]
+  def up
+    add_column :jobs, :priority, :integer, default: nil
+    add_column :deleted_jobs, :priority, :integer, default: nil
+  end
+
+  def down
+    ActiveRecord::Base.transaction do
+      remove_column :jobs, :priority
+      remove_column :deleted_jobs, :priority
+    end
+  end
+end
