5b739260c53395e98265662fbd550d83090c05c0
vijaycel <vvelusamy@celestialsys.com>
2020-05-28 13:20:45 +0530

    Disable ddl transaction

--- db/main/migrate/20200527123653_add_column_priority_to_table_jobs_and_deleted_jobs.rb
+++ db/main/migrate/20200527123653_add_column_priority_to_table_jobs_and_deleted_jobs.rb
@@ -1,4 +1,6 @@
 class AddColumnPriorityToTableJobsAndDeletedJobs < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def up
     add_column :jobs, :priority, :integer, default: nil
     add_column :deleted_jobs, :priority, :integer, default: nil
