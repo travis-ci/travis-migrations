d3ce55004a134188f063bcc5f2114bb319be767e
Maciej Kempin <makemp@users.noreply.github.com>
2021-02-10 15:53:34 +0000

    Update 20210203143155_add_confirmation_fields_to_user.rb

--- db/main/migrate/20210203143155_add_confirmation_fields_to_user.rb
+++ db/main/migrate/20210203143155_add_confirmation_fields_to_user.rb
@@ -3,7 +3,7 @@ class AddConfirmationFieldsToUser < ActiveRecord::Migration[5.2]
 
   def up
     add_column :users, :confirmed_at, :datetime
-    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :token_expires_at, :datetime
     add_column :users, :confirmation_token, :string
 
     add_index :users, :confirmation_token
@@ -11,7 +11,7 @@ class AddConfirmationFieldsToUser < ActiveRecord::Migration[5.2]
 
   def down
     remove_column :users, :confirmed_at
-    remove_column :users, :confirmation_sent_at
+    remove_column :users, :token_expires_at
     remove_column :users, :confirmation_token
   end
 end
