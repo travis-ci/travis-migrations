e87aa62a53eb5218fccaf60fc91a706a568a165b
Artur Kremens <artur.kremens@gmail.com>
2021-03-11 08:46:25 +0100

    Disable set confirmed_at

--- db/main/migrate/20210203143406_confirm_current_users.rb
+++ db/main/migrate/20210203143406_confirm_current_users.rb
@@ -7,9 +7,9 @@ class ConfirmCurrentUsers < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def change
-    time_now = Time.zone.now - 21.days
-    User.all.in_batches do |users|
-      users.update_all(confirmed_at: time_now)
-    end
+    # time_now = Time.zone.now - 21.days
+    # User.all.in_batches do |users|
+    #   users.update_all(confirmed_at: time_now)
+    # end
   end
 end
