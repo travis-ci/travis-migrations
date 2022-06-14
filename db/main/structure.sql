ad21b2c810566afdcad8cf78e70fcd89187dcb6a
gabriel-arc <57348209+GbArc@users.noreply.github.com>
2022-06-14 10:21:58 +0200

    job query optimization ship:docker (#260)

--- db/main/structure.sql
+++ db/main/structure.sql
@@ -724,6 +724,38 @@ CREATE FUNCTION public.most_recent_job_ids_for_user_repositories_by_states(uid i
 
 
 --
+-- Name: most_recent_job_ids_for_user_repositories_by_states_lw(integer, character varying); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION most_recent_job_ids_for_user_repositories_by_states_lw(uid int, states varchar default '') RETURNS table (id bigint)
+  LANGUAGE plpgsql
+  AS $$
+  DECLARE
+  rid int;
+    BEGIN
+      SET LOCAL work_mem = '16MB';
+      IF states <> '' THEN
+        for rid in
+          SELECT repository_id
+          FROM permissions
+          WHERE user_id = uid
+          LOOP
+            RETURN QUERY select j.id from jobs j where repository_id = rid and state in (SELECT unnest(regexp_split_to_array(states, ','))) order by j.id desc limit 100;
+          END LOOP;
+      ELSE
+        for rid in
+          SELECT repository_id
+          FROM permissions
+          WHERE user_id = uid
+          LOOP
+            RETURN QUERY select j.id from jobs j where repository_id = rid order by j.id desc limit 100;
+          END LOOP;
+      END IF;
+    END
+$$;
+
+
+--
 -- Name: set_unique_number(); Type: FUNCTION; Schema: public; Owner: -
 --
 
