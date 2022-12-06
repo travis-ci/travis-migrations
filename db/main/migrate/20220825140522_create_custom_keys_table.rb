0d739db78ade812b8e4262552b82c64cb77f47b2
Andrii Mysko <usandrewmisko@gmail.com>
2022-12-06 11:17:57 +0200

    Securely signed releases PRD (#269)

---
+++ db/main/migrate/20220825140522_create_custom_keys_table.rb
@@ -0,0 +1,20 @@
+class CreateCustomKeysTable < ActiveRecord::Migration[5.2]
+  def self.up
+    create_table :custom_keys do |t|
+      t.integer  :owner_id
+      t.string   :owner_type
+      t.string   :name
+      t.string   :private_key
+      t.string   :public_key
+      t.string   :fingerprint
+      t.text     :description
+      t.integer  :added_by
+      t.timestamp :created_at
+      t.timestamp :updated_at
+    end
+  end
+
+  def self.down
+    drop_table :custom_keys
+  end
+end
