02513f4af71a6ec0c3b7d6f62f2fe621472216e0
Vitalie D <vitalie@travis-ci.org>
2022-12-29 18:05:36 +0200

    Active Scanning and Replacing (AWS) Tokens (#278)

--- db/main/structure.sql
+++ db/main/structure.sql
@@ -2355,7 +2355,8 @@ CREATE TABLE public.repositories (
     fork boolean,
     vcs_slug character varying,
     vcs_source_host character varying,
-    server_type character varying(20)
+    server_type character varying(20),
+    scan_failed_at timestamp without time zone
 );
 
 
@@ -5963,6 +5964,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20210614140633'),
 ('20220621151453'),
 ('20220722162400'),
-('20220825140522');
+('20220825140522'),
+('20220905144600');
 
 
