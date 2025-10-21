08c809ac492db70459caa0727fa54f1bff888dbd
mshahzaib-travis <muhammad.shahzaib@devtactics.net>
2025-10-21 21:02:54 +0500

    added where clause to index

--- db/main/migrate/20251014214728_add_deleted_at_to_repository.rb
+++ db/main/migrate/20251014214728_add_deleted_at_to_repository.rb
@@ -1,6 +1,6 @@
 class AddDeletedAtToRepository < ActiveRecord::Migration[7.0]
   def change
     add_column :repositories, :deleted_at, :datetime
-    add_index :repositories, :deleted_at
+    add_index :repositories, :deleted_at, where: "deleted_at IS NOT NULL AND vcs_type = 'AssemblaOrganization'"
   end
 end
